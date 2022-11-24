89f0102c36413e0c05f2201baaa73ddddd8931e0
Alex <alexfil0606@gmail.com>
2022-11-24 22:27:14 +0300

    exm03

---
+++ example03/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+int[] array = new int[8];
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 101);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int pos = 0;
+    Console.Write("[");
+    while (pos < count)
+    {
+        Console.Write($"{col[pos]}");
+        pos++;
+        if (pos < count) Console.Write(", ");
+    }
+    Console.Write("]");
+}
+
+FillArray(array);
+PrintArray(array);
